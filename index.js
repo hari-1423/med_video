c7100b75436ac61fcd1694cfdb25db62c1d4a48d
hari-1423 <118989825+hari-1423@users.noreply.github.com>
2024-11-02 00:01:37 +0530

    .

--- index.js
+++ index.js
@@ -35,4 +35,4 @@ app.use('/api', videoRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
